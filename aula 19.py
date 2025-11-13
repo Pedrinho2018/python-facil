2edc2855e4a2da7ea06744d48b1beaf613ed3eb8
Pedro Henrique Freitas Silva <pedrohenriksk8@gmail.com>
2025-11-12 23:10:06 -0300

    add novos estudos

---
+++ aula 19.py
@@ -0,0 +1,23 @@
+"""
+Operadores de comparação e lógicos em Python
+operadores  significado    exemplo
+>    maior que     2>1  True
+>=   maior ou igual 2>=2 True
+<    menor que     1<2  True
+<=   menor ou igual 2<=2 True
+==   igual a      2==2 True
+!=   diferente de  2!=3 True
+and  e (todas as condições devem ser verdadeiras) True and False  False
+or   ou (uma das condições deve ser verdadeira)  True or False  True
+not  não (inverte o valor lógico) not True  False                           
+"""
+maior =2>1
+print(maior)
+menor_ou_igual = 2<=2
+print(menor_ou_igual)
+menor_ou_igual2 = 3<=2
+print(menor_ou_igual2)
+igual = 2==2
+print(igual)
+diferente = 2!=3
+print(diferente)    
---
+++ aula17.py
@@ -0,0 +1,15 @@
+condicao1= False
+condicao2= False
+condicao3= False
+condicao4= False
+
+if condicao1:
+    print('Condicao condicao1')
+elif condicao2:
+    print('Condicao condicao2')
+elif condicao3:
+    print('Condicao condicao3')
+elif condicao4:
+    print('Condicao condicao4')
+else:
+    print('Nenhuma condicao foi satisfeita')
