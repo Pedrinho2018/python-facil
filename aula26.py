7538506dc240029f13c35aedc3f2462fe9bb578f
Pedro henrique Freitas silva <90161307+Pedrinho2018@users.noreply.github.com>
2025-11-14 08:24:40 -0400

    Formatação de strings com F-strings

---
+++ aula26.py
@@ -0,0 +1,7 @@
+variavel = 'abc'
+print(f'{variavel}')
+print(f'{variavel:>10}')
+print(f'{variavel:<10}.')
+print(f'{1000.35435321:0=10,.1f}.')
+print(f'hexadecimal de 1500 é {1500:08x}')
+print(f'variavel é {variavel!r}')  # !r chama o repr() da variável
